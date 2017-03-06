285d4f635bb18e64247b8f35bc8258857d96dad1
Harmen Stoppels <harmenstoppels@gmail.com>
2017-03-06 18:12:49 +0100

    Stuff

--- homework_2/src/ex1.m
+++ homework_2/src/ex1.m
@@ -1,39 +1,52 @@
-function ex1(n)
+function ex1(n, mu)
+  close all
   % Let s_i = (i - 1/2)h for i = 1, 2, ..., n.
   % and h := 1 / n the mesh width.
 
   h = 1 / n;
   xs = linspace(h / 2, 1 - h / 2, n).';
 
-  mu_sqrt = 2 * pi;
-  mu = mu_sqrt ^ 2;
-  theta = sin(mu_sqrt * xs);
+  % mu_sqrt = 9 * pi;
+  % mu = mu_sqrt ^ 2;
+  theta = sin(sqrt(mu) * xs);
 
   A = snd_derivative(n, h);
   v = sin(10 * pi * xs);
   v = rand(n, 1);
   v = v / norm(v);
-  jacobi = JacBuck(theta, n, mu);
-
 
   denominators = 10 .^ (1 : 12);
   errors = [];
+  jacobi = JacBuck(theta, n, mu);
   
   for denominator = denominators
     epsilon = 1 / denominator;
     dif = fBuck(theta + epsilon * v, n, mu) - fBuck(theta, n, mu);
-
-    lin = epsilon * jacobi .* [v, A * v] * ones(2, 1);
-
+    lin = epsilon * jacobi * v;
     errors(end + 1) = norm(dif - lin);
   end
 
-  loglog(denominators, 1 ./ denominators.^2, '-+'); hold on;
-  loglog(denominators, errors, '-+');
+  figure('Name','Error');
+  loglog(denominators, 1 ./ denominators .^ 2, '-+'); hold on;
+  loglog(denominators, errors, '-+'); hold off;
+  grid on
+  xlabel('e^{-1}')
+  ylabel('Residual')
+  legend('e^2', 'error')
 
 
-  x = newton(@(x) x^2 - 1, @(x) 2 * x, 0.1, 1e-7, n, mu)
+  % x = newton(@(x, a, b) x^2 - 1, @(x, a, b) 2 * x, 0.1, 1e-4, n, mu)
 
+  pert = rand(n, 1) - 0.5;
+  pert = pert / norm(pert) * norm(theta);
+  theta_0 = theta + 0.0 * pert;
+  [improvement, it] = newton(@fBuck, @JacBuck, theta_0, 1e-9, n, mu);
+
+  figure
+  plot(xs, improvement);
+  title(sprintf('Solution after it = %d', it));
+
+  norm(fBuck(improvement, n, mu))
 end
 
 function fB = fBuck(theta, n, mu)
@@ -42,7 +55,8 @@ function fB = fBuck(theta, n, mu)
 end
 
 function J = JacBuck(theta, n, mu)
-  J = [mu * cos(theta), ones(n, 1)];
+  A = snd_derivative(n, 1 / n);
+  J = A + spdiags(mu * cos(theta), 0, n, n);
 end
 
 function A = snd_derivative(n, h)
@@ -54,8 +68,10 @@ function A = snd_derivative(n, h)
   A(n, n) = -4 / h ^ 2;
 end
 
-function x = newton(f, df, x, tol, p1, p2)
-  while norm(f(x)) > tol
-    x = x - f(x) / df(x)
+function [x, it] = newton(f, df, x, tol, p1, p2)
+  it = 0;
+  while norm(f(x, p1, p2), 2) > tol
+    x = x - df(x, p1, p2) \ f(x, p1, p2);
+    it = it + 1;
   end
 end
