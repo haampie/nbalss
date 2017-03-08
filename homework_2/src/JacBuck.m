bb71e20245f72fca008295e855c371022ae24cf6
Harmen <harmenstoppels@gmail.com>
2017-03-08 20:27:11 +0100

    Fix h^2 = 1 / (n + 1)^2

--- homework_2/src/JacBuck.m
+++ homework_2/src/JacBuck.m
@@ -1,5 +1,6 @@
 function J = JacBuck(theta, n, mu)
   A = linBuck(n);
-  h_inv = (n + 1)^2;
-  J = A + spdiags(mu * cos(theta) / h_inv, 0, n, n);
+  % h ^ 2 = 1 / (n + 1) ^ 2
+  diag_vec = mu * cos(theta) / (n + 1) ^ 2;
+  J = A + spdiags(diag_vec, 0, n, n);
 end
--- homework_2/src/fBuck.m
+++ homework_2/src/fBuck.m
@@ -1,6 +1,6 @@
 function f = fBuck(theta, n, mu)
   A = linBuck(n);
 
-  % Here h = 1 / (n + 1)^2
-  f = A * theta + mu * sin(theta) / (n + 1)^2;
+  % Here h ^ 2 = 1 / (n + 1) ^ 2
+  f = A * theta + mu * sin(theta) / (n + 1) ^ 2;
 end
