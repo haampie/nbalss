200ed15271b81ec949d92b623828d84323413d4f
Harmen Stoppels <harmenstoppels@gmail.com>
2017-05-19 14:04:27 +0200

    Plaatjes

--- practical/plot_QG/plotQG.m
+++ practical/plot_QG/plotQG.m
@@ -1,4 +1,4 @@
-function plotQG(directory)
+function plotQG(directory, nr)
 
   figure;
 
@@ -6,13 +6,16 @@ function plotQG(directory)
   % resolution 
   n = 128;
   m = 128;
+  
   % read solution
   [lab icp par xl xlp sig sol solup soleig]  = readfort3(n,m,[directory 'fort.3']);
+  
   % constants 
   udim = 1.6e-02; 
   ldim = 1.0e+06; 
   hdim = 6.0e+02; 
   fact = udim*hdim*ldim/1.0e+06; 
+  
   % grid
   for i=1:n
      x(i) = (i-1)/(n-1);
@@ -20,13 +23,21 @@ function plotQG(directory)
   for j=1:m
      y(j) = (j-1)/(m-1); 
   end
+
   % scaling
-  maxp = max(max(sol(:,:,2))) 
+  maxp = max(max(sol(:, :, nr))) 
+
+  % titles = {
+  %   '\psi',
+  %   '\zeta'
+  % };
+  
   % plot
-  plot2D_zs(x,y,fact*sol(:,:,2)/maxp,10,1); 
-  xlabel('x/L','FontSize',20)
-  ylabel('y/L','FontSize',20)
-  title('')
+  plot2D_zs(x,y,fact*sol(:, :, nr) / maxp, 10, 1); 
+  xlabel('x/L')
+  ylabel('y/L')
+  % title(titles{nr})
+
   saveeps(15,10, strcat(directory, 'Fig3d.eps'))
 end
  
